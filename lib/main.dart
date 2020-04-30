2915d2c2d07d9f4cddd67bb2f10dd4171fb9ccaf
sarvesh2000 <sarvesh4232@gmail.com>
2020-04-30 16:10:30 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//Program Starts from main()
+void main() {
+  runApp(
+    //Runs the mentioned module
+    MaterialApp(
+      //Module that contains Material Design Widgets
+      home: Center(
+        //Module that centers items
+        child: Text("Hello World"), //Plain Text
+      ),
+    ),
+  );
+}
